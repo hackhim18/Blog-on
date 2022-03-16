ee89fcd85f0947f00e7f77208445769df9b19f00
hackhim18 <triphimanshu18@gmail.com>
2022-03-16 23:36:23 +0530

    c2-16/03

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Home from './pages/home/Home';
 import Topbar from './components/topbar/TopBar';
 import Single from './pages/single/single';
 import Write from './pages/write/Write';
+import Settings from './pages/settings/settings';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 
 function App() {
   return (
     <div className='App'>
      <Topbar />
-      <Write />
+    <Login /> 
+    
     </div>
   )
 }
