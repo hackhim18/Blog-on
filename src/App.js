336b0882c675769cc71ef0598eefdcd3cdc56b92
hackhim18 <triphimanshu18@gmail.com>
2022-03-17 00:17:21 +0530

    c1-17/03

--- src/App.js
+++ src/App.js
@@ -6,16 +6,42 @@ import Write from './pages/write/Write';
 import Settings from './pages/settings/settings';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-
+import { Routes } from "react-router-dom";
+import { BrowserRouter as Router,Link,Route } from "react-router-dom";
 
 function App() {
+  
   return (
-    <div className='App'>
-     <Topbar />
-    <Login /> 
-    
-    </div>
-  )
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route exact path="/">
+         <Home />
+        </Route>
+     
+        <Route path="/register">
+         <Register />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/write">
+          <Write />
+        </Route>
+        
+        <Route path="/settings">
+           <Settings /> 
+        </Route>
+        <Route path="/post/:postId">
+           <Single /> 
+        </Route>
+
+
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;
+export default App; 
